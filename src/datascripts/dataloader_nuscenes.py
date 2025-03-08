06874c76a3334c34b336419dcb29a9f814f6a618
Thanh <truongthanh@u.nus.edu>
2025-03-08 17:58:13 +0800

    Add angle to lane (might break pre-processing script)

--- src/datascripts/dataloader_nuscenes.py
+++ src/datascripts/dataloader_nuscenes.py
@@ -744,6 +744,7 @@ class NuScenesData(SingleAgentDataset):
                     print(f'lane_midline_rel: {lane_midline_rel}')
                     print(f'tmp_lane_midline_abs: {tmp_lane_midline_abs}')
                     print(f'tmp_lane_midline_rel: {tmp_lane_midline_rel}')
+                    print(f'lane_traj_tokens[lane_idx]: {lane_traj_tokens[lane_idx]}')
                     print_count += 1
                 self.valid_lanes_midline_rel.append(np.array(tmp_lane_midline_rel))
                 self.valid_lanes_midline_abs.append(np.array(tmp_lane_midline_abs))
