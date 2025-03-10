f14eac30e479e64335321559799d9b7400dcb705
Thanh <truongthanh@u.nus.edu>
2025-03-10 15:27:10 +0800

    Add lane angle to decoder

--- src/modeling/laplace_decoder.py
+++ src/modeling/laplace_decoder.py
@@ -256,7 +256,9 @@ class GRUDecoder(nn.Module):
             dense_lane_topk_scores[i][:k] = dense_lane_pred[i][topk_idxs] # [N*H, mink]
 
             # TODO: verify this works
-            dense_lane_topk_lane_meta.append(subdivided_lane_to_lane_meta[i][topk_idxs.tolist()])
+            dense_lane_topk_lane_meta.append(
+                [subdivided_lane_to_lane_meta[i][index] for index in topk_idxs.tolist()]
+            )
 
         print(f'-------------------------------------------------')
         for idx in random_idxs:
